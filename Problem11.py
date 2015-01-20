ebffdd583a2cfa2b4588807d37b5cb9c723d2d89
Kirsten Korevaar <kirsten@eml.cc>
2015-01-20 14:22:53 +0700

    Add structure and outline

---
+++ Problem11.py
@@ -0,0 +1,15 @@
+# problem 11
+# What is the greatest product of four adjacent numbers in the same direction (up, down, left, right, or diagonally) in the 20×20 grid?
+# 20 * 20 grid
+# We are looking for four number in a row that MULTIPLIED ive the biggest number
+# Note: Does this mean that the number added also give the biggest number? Is there a computational advantage?
+# We do not need to know the four numbers just the result
+
+# store the result and compare
+result = 0 
+
+# check all rows
+
+# check all columns
+
+# check all diagonals
