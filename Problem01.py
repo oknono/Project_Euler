180d1897f260834b915a637a0e40565e1a40e930
Kirsten Korevaar <kirsten@eml.cc>
2021-08-06 16:10:07 +1000

    update code to python3

--- Problem01.py
+++ Problem01.py
@@ -1,12 +1,21 @@
 # Update using PDF from Euler Website 28 September
 # This works with larger numbers as well!
-# Note that 3 + 6 + 9 + ... is equal to
-# 3 * (1 + 2 + 3 + ...) etc.
+# Two importan steps:
+# 1. sum of all numbers divisible by x or y is equal to
+#   ( sum of all numbers divisible by x +
+#	sum of all numbers divisible by x ) -
+#	sum of all numbers divisible by x * y
+# 2. The sum of all multiples of x can be rewritten from
+# 	- [x, 2x, 3x, 4x, ...] to
+#   - x * [1,2,3,4... floor(target / x) ]
+import math
 
-target = 999999999
+target = 999
 
-def sum_divided_by(n):
-	reduced_number = target / n
-	return n * (reduced_number * (reduced_number + 1)) / 2
 
-print sum_divided_by(3) + sum_divided_by(5) - sum_divided_by(15)
+def sum_divisible_by(n):
+    p = math.floor(target / n)  # step 2
+    return n * ((p * (p + 1)) / 2)
+
+
+print(sum_divisible_by(3) + sum_divisible_by(5) - sum_divisible_by(15))  # step 1
